a21f3797a9b0b0bdb6ecc030d62905df6012895d
anthonyTorres Vargas <anthonytorresvar@hotmail.com>
2021-02-18 18:09:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.nuestraAgrupacion', { delay: 500 });
+ScrollReveal().reveal('.news-card', { delay: 500 });
+ScrollReveal().reveal('.Integrantes', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.footer-links', { delay: 500 });
